cea0cca4bf35978ceb5916b25f85d5460adabf8c
raghav-1998 <raghav.vasdeva@gmail.com>
2023-03-24 17:47:33 +0530

    Playing with Arithemtic Operators

---
+++ Operators/Operators.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  // Arithemtic Operators
+  print(2 + 3); //Adding 2 numbers
+  print(2 - 3); // Subtracting 2 numbers
+  print(2 * 3); // Multiplying 2 numbers
+  print(2 / 3); // Dividing 2 numbers & return divisiondouble
+  print(-(2 - 3)); // Unary minus
+  print(2 ~/ 3); // return quotient after division(Integer division)
+  print(2 % 3);  // returning modulus of 2 number
+}
