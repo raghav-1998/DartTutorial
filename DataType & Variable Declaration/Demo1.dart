c77b2aa446012ab58a2ee693b1b162db2aba92cc
raghav-1998 <raghav.vasdeva@gmail.com>
2023-03-24 17:27:17 +0530

    Adding First Demo of Data Type

---
+++ DataType & Variable Declaration/Demo1.dart
@@ -0,0 +1,21 @@
+void main() {
+  //Decalaration with datatype
+  int number1 = 10; //Integer variable
+  double percentage = 85.346;
+  bool flag1 = false;
+  String company = "Infosys";
+
+  // Declaration without datatype
+  var name = "Raghav"; //String variable
+  var number = 10; //Integer variable
+  var pi = 3.14; // Double variable
+  var flag = true; //boolean variable
+
+  var state;
+  print(state); //Value of uninitialized variable is null as these are objects
+
+  print("My name is ${name}. I am working in ${company}"); //String Interpolation
+
+  
+
+}
